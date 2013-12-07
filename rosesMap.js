3a8a8bd6d25747f2a09e8fc95c6b944053548de5
Lyzi Diamond <lyzidiamond@gmail.com>
2013-12-06 16:24:48 -0800

    i can haz html? try 6

--- rosesMap.js
+++ rosesMap.js
@@ -1,28 +1,38 @@
+// text for attribution
 var attribution = '<a href="http://leafletjs.com/">Leaflet</a>, <a href="http://stamen.com">Stamen</a>, rose designed by Nithin Viswanathan from the <a href="http://nounproject.com">Noun Project</a>';
 
-var map = L.map('roses-map').setView([45.528479,-122.670014], 15);
+// initialize map, set initial center, set initial zoom
+var map = L.map('roses-map').setView([45.528479,-122.670014], 12);
 
+// define tiles, set max zoom, apply attribution, add to map
 L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
   maxZoom: 18,
   attribution: attribution
 }).addTo(map);
 
+// create rose map marker
 var roseIcon = L.icon({
   iconUrl: 'rose.png',
   iconSize: [90, 90]
 });
 
+// define popup text, add to each layer
 function onEachFeature(feature, layer) {
   var popupContent = "<a href='" + feature.properties.tumblrUrl + "'>" + feature.properties.name + "</a>";
   layer.bindPopup(popupContent);
-}
+};
 
-L.geoJson('/roses.geojson', {
-  pointToLayer: function (feature, latlng) {
-    return L.marker(latlng, {icon: roseIcon});
-  },
-  onEachFeature: onEachFeature
-}).addTo(map);
+// add geojson data, apply rose map markers, apply popups, add to map
+$.getJSON('./roses.geojson', function(data) {
+  var geojson = L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, {roseIcon});
+    },
+    onEachFeature: onEachFeature
+  });
+  geojson.addTo(map);
+  map.fitBounds(geojson.getBounds());
+});
 
 /*
 
